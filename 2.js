8eea23e143174f525dd610855ba0861670ad4410
Aditya <adityavishal903@gmail.com>
2023-06-27 09:11:43 +0530

    2

--- 2.js
+++ 2.js
@@ -1,3 +1,10 @@
 const fs= require("fs");
-const text= fs.readFileSync("aditya.txt", "utf-8");
+let text= fs.readFileSync("aditya.txt", "utf-8");
 console.log(text);
+
+text= text.replace("content","Aditya");
+console.log("this content of the filled is");
+console.log(text);
+
+console.log("Creating a new file...")
+fs.writeFileSync("raj.txt", text);
